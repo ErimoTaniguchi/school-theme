a510e51bc5ef64deb827d3a9f701e891bf328004
k-e-r <damajonboua@gmail.com>
2026-09-11 01:05:55 -0700

    Fix refine layout and block styling

--- functions.php
+++ functions.php
@@ -123,6 +123,18 @@ function enqueue_lightgallery_assets() {
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_lightgallery_assets' );
 
+// Make custom sizes selectable from WordPress admin.
+function school_add_custom_image_sizes( $size_names ) {
+	$new_sizes = array(
+		'400x500' => __( '400x500', 'school-theme' ),
+		'200x250' => __( '200x250', 'school-theme' ),
+		'400x200' => __( '400x200', 'school-theme' ),
+		'800x400' => __( '800x400', 'school-theme' ),
+	);
+	return array_merge( $size_names, $new_sizes );
+}
+add_filter( 'image_size_names_choose', 'school_add_custom_image_sizes' );
+
 // Load custom blocks.
 require get_theme_file_path() . '/school-blocks/school-blocks.php';
 
