47d8e9cf492fd2343bf0c220b39b703ca757aa20
k-e-r <damajonboua@gmail.com>
2026-09-14 14:41:51 -0700

    Fix AOS editor visibility and default animation

--- school-blocks/build/school-blocks/index.asset.php
+++ school-blocks/build/school-blocks/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a4ba74a73c0f65c585b5');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '717b03c771a1be40366c');
