8841d9fa07470085e67351ae6efdf787cdf04529
k-e-r <damajonboua@gmail.com>
2026-09-14 15:07:00 -0700

    Fix horizontal overflow caused by AOS animations

--- school-blocks/build/school-blocks/index.asset.php
+++ school-blocks/build/school-blocks/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '717b03c771a1be40366c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '27d5e82bc54e7c6afa4d');
